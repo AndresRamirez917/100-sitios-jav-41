628c13e4597eeedd8d4c23a815bb753fc43744a5
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-08 18:54:54 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -31,4 +31,44 @@ async function getData() {
     });
 }
 
+const emailValido = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)
+}
+const btn_validar = document.getElementById('btn-validar').onclick = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje');
+    const arr = [];
+    const messageArr = ["Nombre", "Email", "Mensaje"];
+    arr.push(nombre, email, mensaje);
+    for( i = 0; i < arr.length; i++){
+        if(arr[i].value == ""){
+            swal({
+                title: `El campo ${messageArr[i]} no puede estar vacío`,
+                icon: "error",
+                 })
+                 return false;
+        }
+    }
+
+    if(!emailValido(email)){
+        swal({
+            title: `El campo ${messageArr[1]} no tiene el formato adecuado`,
+            icon: "error",
+             })
+             return false;
+    }
+    swal({
+        title: `Datos enviados satisfactoriamente`,
+        icon: "success",
+         })
+         nombre.value = "";
+         email.value = "";
+         mensaje.value = "";
+         return true;
+}
+
+
+
 getData()
